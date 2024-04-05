449770d7bdfd43c69fe140bbf96768c7f98f6ab2
Micael Santiago <santiagoo.micael04@gmail.com>
2024-04-04 21:33:39 -0300

    feat: add router

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import express, { json } from 'express';
+import { router } from './router.js';
 
 const app = express();
 const PORT = process.env.PORT || 3333;
 
 app.use(json());
+app.use(router);
+
 app.listen(PORT, () => console.log(`Server running on the port ${PORT}`));
